c60c4762ac2168f9739939e2ea3520f1272d52f0
cgilliard <christopher.gilliard@gmail.com>
2019-08-30 15:39:04 -0700

    federation

--- src/main/java/servlets/sender.java
+++ src/main/java/servlets/sender.java
@@ -1,11 +1,13 @@
 package servlets;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.DataOutputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,14 +20,21 @@ public class sender extends HttpServlet
      * 
      */
     public static final long serialVersionUID = 1L;
+    public static final String DEFAULT_MWCMQS_DOMAIN = "mqs.mwc.mw";
     
     private AsyncCompletion acomp = null;
-    
+    private String domain = null;
+
     public void init()
     {
         acomp = listener.acomp;
+        String suggest = getServletConfig().getInitParameter("mwcmqs_domain");
+        if(suggest != null)
+            domain = suggest;
+        else
+            domain = DEFAULT_MWCMQS_DOMAIN;
     }
-    
+
     private static Logger log = Logger.getLogger("mwcmq2");
 
     public void doPost(HttpServletRequest req, HttpServletResponse res)
@@ -44,20 +53,57 @@ public class sender extends HttpServlet
         {
             String address = req.getParameter("address");
             String message = jb.toString();
-            System.out.println("message: " + message);
-            long lastSeen = System.currentTimeMillis() - acomp.getLastSeenTime(address);
             
-            System.out.println("recipient last seen " + lastSeen + " ms ago.");
-            
-            acomp.send(address, message);
-            pw = res.getWriter();
+            boolean is_target_domain = true;
+            String target_domain = DEFAULT_MWCMQS_DOMAIN;
+            if(address.indexOf("@")>=0) {
+                target_domain = address.substring(address.indexOf(address.indexOf("@")+1));
+                if(!target_domain.equalsIgnoreCase(domain))
+                    is_target_domain = false;
+            }
+            else if(!domain.equalsIgnoreCase(DEFAULT_MWCMQS_DOMAIN))
+            {
+                is_target_domain = false;
+            }
 
-            //writing html in the stream
-            pw.print("lastSeen: "+lastSeen);
-            
+            if(is_target_domain)
+            {
+                long lastSeen = System.currentTimeMillis() -
+                        acomp.getLastSeenTime(address);
+                acomp.send(address, message);
+                pw = res.getWriter();
+                //writing html in the stream
+                pw.print("lastSeen: "+lastSeen);
+            }
+            else
+            {
+                // it's another server so we need to forward.
+                // for now just do a synchronous connection.
+                // Maybe move to thread later.
+                
+                String url = "https://" + target_domain + "/sender?address=" + address;
+                URL obj = new URL(url);
+                HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+                //add reuqest header
+                con.setRequestMethod("POST");
+                con.setRequestProperty("User-Agent", "mwcmqs 1.0");
+                con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+                // Send post request
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(message);
+                wr.flush();
+                wr.close();
+
+                int responseCode = con.getResponseCode();
+                System.out.println("Resp " + responseCode);
+                pw = res.getWriter();
+                pw.println("LastSeen: Unknown");
+            }
+
             log.log(Level.INFO, "sender");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch (Exception e) {
             e.printStackTrace();
         }
         finally
