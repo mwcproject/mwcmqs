9737ae91a2cf4709449cf7468c531f189ef62b44
cgilliard <christopher.gilliard@gmail.com>
2019-09-04 02:06:38 -0400

    timenow servlet

---
+++ src/main/java/servlets/timenow.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class timenow extends HttpServlet
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1026537920201200646L;
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+    {
+        try {
+            String timeNow = Long.toString(System.currentTimeMillis());
+            res.getOutputStream().write(timeNow.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
