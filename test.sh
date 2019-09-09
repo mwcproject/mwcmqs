963249fa8c7f929a4ceb5fd2c404ffd50d404480
cgilliard <christopher.gilliard@gmail.com>
2019-09-09 18:44:45 -0400

    test improvement

--- test.sh
+++ test.sh
@@ -4,4 +4,7 @@ cd "$(dirname "$0")"
 mvn install -DskipTests=true
 rm -rf jetty/webapps/root/WEB-INF/lib/javax.servlet-api-4.0.0-b07.jar
 cp jetty/log4j.properties jetty/webapps/root/WEB-INF/classes
+cp jetty/webapps/root/WEB-INF/web.xml jetty/webapps/root/WEB-INF/web.xml.bak
+cp resources/web.xml jetty/webapps/root/WEB-INF
 mvn test
+mv jetty/webapps/root/WEB-INF/web.xml.bak jetty/webapps/root/WEB-INF/web.xml
